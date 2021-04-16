3965d73a896334b54901af2d96bac9f6df715aaa
Changhyun Kim <movementfor@gmail.com>
2021-04-16 22:59:03 +0100

    Sync

--- data_reader.py
+++ data_reader.py
@@ -4,43 +4,35 @@ import numpy as np
 import math
 
 
-def read():
-    # root = os.getcwd()
-    # infile = os.path.join(root, 'dataset', infile)
-    # file_name = infile.split('/')[-1]
-    # print(f"Loading data from file {infile}...\n")
-    # with open(infile, 'r') as f:
-    #     data = pd.read_csv(infile)
-
+def read_data(window=22):
     price = pd.read_csv('dataset/kospi.csv')
     oil = pd.read_csv('dataset/oilprice.csv')
-    gold = pd.read_csv('dataset/goldprice.csv')
+    # gold = pd.read_csv('dataset/goldprice.csv')
     data = pd.DataFrame()
     data['Date'] = price['Date']
     data['Daily_trading_range'] = price['High'] - price['Low']
-    data['Log_Volume_change'] = np.log((price['Volume'] / price['Volume'].shift(1)))*100
+    data['Log_Volume_change'] = np.log((price['Volume'] / price['Volume'].shift(1))) * 100
     data['Daily_return'] = price['Close'].pct_change().dropna()
     data['Daily_log_return'] = np.log(price['Close'] / price['Close'].shift(1))
     data['Index'] = price['Close']
-    data['Past_vol22'] = np.sqrt((data['Daily_log_return']**2).rolling(window=22).std()) * np.sqrt(252)
-
-    data['gold'] = gold['Close']
+    # data['gold'] = gold['Close']
     data['oil'] = oil['Close']
+    data = data.dropna().reset_index(drop=True)
 
-    volatility = np.sqrt((data['Daily_log_return']**2).rolling(window=22).sum()/22)*np.sqrt(252)
-    vol10 = np.sqrt((data['Daily_log_return']**2).rolling(window=10).sum()/10)*np.sqrt(252)
+    data = data.iloc[:-window]
+    volatility = np.sqrt((data['Daily_log_return'] ** 2).rolling(window=window).sum() / window) * np.sqrt(252)
+
     # target = yz_vol_measure(data)
     # target10 = yz_vol_measure(data, window=10)
-    target22 = pd.DataFrame(volatility)
-    target10 = pd.DataFrame(vol10)
-
-    data['Target22'] = target22
-    data['Target10'] = target10
-    # data = data.dropna()
+    target = pd.DataFrame()
+    target['Target'] = volatility[window:].reset_index(drop=True)
+    # target['Target'] = volatility[22:].reset_index(drop=True)
+    target = target.dropna()
 
     # data['Target10'] = target10
+    # data = data.dropna()
 
-    return data
+    return data, target
 
 # This method refers to Yang-Zhang volatility measure for making ground truth of the volatility
 def yz_vol_measure(data, window=22, trading_periods=252):
